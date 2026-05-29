316f4a45e1afe856a1a7d378258c2e1ab296a4d2
Rajat Kumar Majumdar <rdmajumdar007@gmail.com>
2026-05-29 11:13:12 +0000

    configure config

---
+++ 01_basics/test.js
@@ -0,0 +1,13 @@
+console.log("Hello world")
+console.log(3+"3")
+
+const studentID ="34as56"
+let studentName = " Rajat "
+var studentEmail = " rajat@google.com"
+var studentAddress;
+studentEmail = "newplottwist"
+
+console.table([studentName,studentEmail,studentAddress])
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof studentName)
